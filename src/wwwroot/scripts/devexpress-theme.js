28dd57e366212b9c85be98f1fb9e733ba4f4098f
Tamás Mikola <73854810+TamasMikola@users.noreply.github.com>
2024-02-21 12:14:58 +0100

    8.3.0 (#5)
    * 8.2.1
    
    * rename FormComponent to BaseFormComponent
    
    * rename GridComponent to BaseGridComponent
    
    * Add FormComponent and  GridComponent parameters
    
    * Lazy load for embedded grids
    
    * EventDispatcher
    
    * fix: date filter
    
    * AppRootPath
    
    * CustomMenu
    
    * readme
    
    * ver 8.3

--- src/wwwroot/scripts/devexpress-theme.js
+++ src/wwwroot/scripts/devexpress-theme.js
@@ -1,8 +1,8 @@
-﻿export function setTheme(theme, appRootUrl) {
+﻿export function setTheme(theme, appRootPath) {
     var body = document.getElementsByTagName('body')[0];
     body.style.display = 'none';
     body.classList.add(theme);
-    document.getElementById('devexpress-theme').href = `${appRootUrl}_content/DevExpress.Blazor.Themes/${theme}.min.css`;
+    document.getElementById('devexpress-theme').href = `${appRootPath}_content/DevExpress.Blazor.Themes/${theme}.min.css`;
     setTimeout(function () {
         body.style.display = '';
     }, 500);
