159243dd4fc4b81042cc9eb25a0d88522ca8b687
Tamás Mikola <73854810+TamasMikola@users.noreply.github.com>
2025-01-31 16:47:20 +0100

    8.16.0 (#21)

--- src/wwwroot/scripts/devexpress-theme.js
+++ src/wwwroot/scripts/devexpress-theme.js
@@ -2,7 +2,7 @@
     var body = document.getElementsByTagName('body')[0];
     body.style.display = 'none';
     body.classList.add(theme);
-    document.getElementById('devexpress-theme').href = `${appRootPath}_content/DevExpress.Blazor.Themes/${theme}.min.css`;
+    document.getElementById('devexpress-theme').href = `${appRootPath}/_content/DevExpress.Blazor.Themes/${theme}.min.css`;
     setTimeout(function () {
         body.style.display = '';
     }, 500);
